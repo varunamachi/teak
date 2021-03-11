00f2066c65b0807d8a045e7c33eed810e078a3d6
Varuna Amachi <varunamachi@gmail.com>
2021-03-11 22:24:02 +0530

    Allow setting default mongo database

--- mg/conn.go
+++ mg/conn.go
@@ -76,104 +76,15 @@ func Connect(gtx context.Context, opts *ConnOpts) error {
 // C - get a handle to collection in the default database, single letter name
 // to have nice way to transition from mgo
 func C(collectionName string) *mongo.Collection {
-	return CcollectionWithDB(defaultDB, collectionName)
+	return CollectionWithDB(defaultDB, collectionName)
 }
 
-// CcollectionWithDB - gives a reference to a collection in given database
-func CcollectionWithDB(db, coll string) *mongo.Collection {
+// CollectionWithDB - gives a reference to a collection in given database
+func CollectionWithDB(db, coll string) *mongo.Collection {
 	return mongoStore.client.Database(db).Collection(coll)
 }
 
-// //Conn - represents a mongdb connection
-// type Conn struct {
-// 	*mongo.Collection
-// }
-
-// //SetDefaultDB - sets the default DB
-// func SetDefaultDB(defDB string) {
-// 	defaultDB = defDB
-// }
-
-// //Close - closes mongodb connection
-// func (conn *Conn) Close() {
-// 	conn.Database().Close()
-// }
-
-// //toOptStr - converts mongodb options to a string that can be used as URL to
-// //connect to a mongodb instance
-// func toOptStr(options []*ConnOpts) string {
-// 	var buf bytes.Buffer
-// 	for i, opt := range options {
-// 		//userName:password@host:port[,userName:password@host:port...]
-// 		//buf.WriteString("mongo://")
-// 		if len(opt.User) != 0 {
-// 			buf.WriteString(opt.User)
-// 			buf.WriteString(":")
-// 			buf.WriteString(opt.Password)
-// 			buf.WriteString("@")
-// 		}
-// 		if len(opt.Host) != 0 {
-// 			buf.WriteString(opt.Host)
-// 		} else {
-// 			buf.WriteString("localhost")
-// 		}
-
-// 		if opt.Port != 0 {
-// 			buf.WriteString(":")
-// 			buf.WriteString(strconv.Itoa(opt.Port))
-// 		}
-// 		if len(options) > 1 && i < len(options)-1 {
-// 			//In case of multiple addresses
-// 			buf.WriteString(",")
-// 		}
-// 	}
-// 	co := buf.String()
-// 	return co
-// }
-
-// //ConnectSingle - connects to single instance of mongodb server
-// func ConnectSingle(opts *ConnOpts) (err error) {
-// 	err = Connect([]*ConnOpts{opts})
-// 	if err == nil {
-// 		teak.Info("DB:Mongo", "Connected to mongo://%s:%d",
-// 			opts.Host, opts.Port)
-// 	} else {
-// 		teak.Error("DB:Mongo", "Failed to connected to mongo://%s:%d",
-// 			opts.Host, opts.Port)
-// 	}
-// 	return err
-// }
-
-// //Connect - connects to one or more mirrors of mongodb server
-// func Connect(opts []*ConnOpts) (err error) {
-// 	var sess *mgo.Session
-// 	optString := toOptStr(opts)
-// 	sess, err = mgo.Dial(optString)
-// 	if err == nil {
-// 		sess.SetMode(mgo.Monotonic, true)
-// 		mongoStore = &store{
-// 			session: sess,
-// 			opts:    opts,
-// 		}
-// 		teak.Info("DB:Mongo", "Connected to mongoDB")
-// 	}
-// 	return teak.LogError("DB:Mongo", err)
-// }
-
-// //NewConn - creates a new connection to mogodb
-// func NewConn(dbName string) (conn *Conn) {
-// 	conn = &Conn{
-// 		Database: mongoStore.session.Copy().DB(dbName),
-// 	}
-// 	return conn
-// }
-
-// //DefaultConn - creates a connection to default DB
-// func DefaultConn() *Conn {
-// 	return NewConn(defaultDB)
-// }
-
-// //CloseConn - closes the mongodb connection
-// func CloseConn() {
-// 	mongoStore.session.Close()
-// }
+//SetDefaultDB - sets the default DB
+func SetDefaultDB(defDB string) {
+	defaultDB = defDB
+}
